633b24bbecdce18b7a1aa9ced4c39b4655203020
TrollRoll2 <patrik.backman@helsinki.fi>
2026-01-17 05:18:13 +0200

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,13 +4,19 @@ const Person = ({name, number}) => <div>{name} {number}</div>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleInput = (event) => setNewName(event.target.value)
   const handleInputNumber = (event) => setNewNumber(event.target.value)
+  const handleInputFilter = (event) => setNewFilter(event.target.value)
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
 
   const handleName = (event) => {
     event.preventDefault()
@@ -26,6 +32,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <h2>Add a person</h2>
       <form onSubmit={handleName}>
         <div>
           name: <input
@@ -41,8 +48,13 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+      <div>
+        Filter numbers: <input
+        value={newFilter}
+        onChange={handleInputFilter} />
+      </div>
       <h2>Numbers</h2>
-        {persons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
+        {filteredPersons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
     </div>
   )
 }
