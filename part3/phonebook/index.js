8ecf0c98bf045155cf85008de986a8faa0806fc3
TrollRoll2 <patrik.backman@helsinki.fi>
2026-01-20 08:33:40 +0200

    exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -30,6 +30,14 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  request.date = new Date()
+  response.send(`
+    <div>Phonebook has info for ${persons.length} people </div>
+    <div>${request.date}</div>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
