a56a9f4d1b454759e9362647b30bfd2414a3a721
TrollRoll2 <patrik.backman@helsinki.fi>
2026-01-22 20:02:20 +0200

    exercise 3.13-14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,29 +1,11 @@
+console.log("Node version:", process.version);
+console.log("Node path:", process.execPath);
+
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -37,28 +19,17 @@ morgan.token('body', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find(({})).then(persons => response.json(persons))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find((p) => p.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(p => response.json(p))
+    .catch(response.status(404).end())
 })
-
-const generateId = () => {
-  const maxId = Math.floor(Math.random() * 1000)
-  return String(maxId + 1)
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  const exists = persons.find((p) => p.name === body.name)
 
   if ((!body.name || !body.number)) {
     return response.status(400).json({ 
@@ -66,21 +37,14 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (exists) {
-    return response.status(400).json({
-      error: 'person already exists in phonebook'
-    })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(saved => {
+    response.json(saved)
+  })
 })
 
 
